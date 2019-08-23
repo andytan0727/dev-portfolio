fbd6fc4cb52418728cd7f52ad3075b2a84118803
Andy Tan <andytan0727@gmail.com>
2019-08-23 22:51:42 +0800

    feat: add scrollbar style to body

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,16 +2,48 @@ import "normalize.css";
 
 import React from "react";
 import { Box } from "rebass";
-import { ThemeProvider } from "theme-ui";
+import { ThemeProvider, useThemeUI } from "theme-ui";
 
 import Footer from "@components/footer";
 import Header from "@components/header";
 import SEO from "@components/seo";
+import { css, Global } from "@emotion/core";
 import theme from "@src/gatsby-plugin-theme-ui";
 
 interface LayoutProps {
   children: React.ReactChildren | React.ReactElement;
 }
+
+const GlobalStyles = () => {
+  const {
+    theme: {
+      colors: { primary, text },
+    },
+  } = useThemeUI();
+
+  return (
+    <Global
+      styles={css`
+        body {
+          &,
+          & * {
+            &::-webkit-scrollbar {
+              width: 10px;
+            }
+
+            &::-webkit-scrollbar-thumb {
+              background: ${primary};
+            }
+
+            &::-webkit-scrollbar-track {
+              background-color: ${text};
+            }
+          }
+        }
+      `}
+    />
+  );
+};
 
 const Layout: React.FunctionComponent<LayoutProps> = ({
   children,
@@ -20,8 +52,9 @@ const Layout: React.FunctionComponent<LayoutProps> = ({
     <>
       <Header />
       <ThemeProvider theme={theme}>
-        <Box as="main" mt={2} px={4} width={1} height="100%">
+        <Box as="main" width={1}>
           <SEO />
+          <GlobalStyles />
           {children}
         </Box>
       </ThemeProvider>

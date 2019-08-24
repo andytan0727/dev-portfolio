46a168991c73fc7b29cb0badfc480286374e114b
Andy Tan <andytan0727@gmail.com>
2019-08-24 22:48:26 +0800

    feat: create progress bar component

---
+++ src/components/progress/progress-bar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Box } from "rebass";
+
+interface ProgressBarProps {
+  trackHeight?: number;
+  percent: number;
+}
+
+const ProgressBar: React.FunctionComponent<ProgressBarProps> = (
+  props: ProgressBarProps
+) => {
+  const { trackHeight, percent } = props;
+
+  if (percent > 100 || percent < 0)
+    throw new Error("Percentage must be in between 0 and 100");
+
+  const barWidth = percent / 100;
+  const height = trackHeight || 20;
+
+  return (
+    <Box
+      className="progress-bar"
+      height={height}
+      sx={{
+        backgroundColor: "text",
+      }}
+    >
+      <Box
+        width={barWidth}
+        height={height}
+        className="progress-track"
+        sx={{
+          backgroundColor: "primary",
+        }}
+      ></Box>
+    </Box>
+  );
+};
+
+export default ProgressBar;

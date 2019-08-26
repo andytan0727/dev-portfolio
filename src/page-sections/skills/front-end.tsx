200e89ab207a40166a565f99d2d59e1ac65f148c
Andy Tan <andytan0727@gmail.com>
2019-08-26 09:16:56 +0800

    feat: add several skills to skills section
    - JavaScript
    - Html
    - Css
    - Vue

--- src/page-sections/skills/front-end.tsx
+++ src/page-sections/skills/front-end.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import {
   DiAngularSimple,
   DiBootstrap,
+  DiCss3,
+  DiHtml5,
+  DiJavascript1,
   DiJqueryLogo,
   DiReact,
   DiSass,
 } from "react-icons/di";
+import { FaVuejs } from "react-icons/fa";
 import { Box, Text } from "rebass";
 
 import { TypescriptSvg } from "@components/image";
@@ -18,16 +22,41 @@ const FrontEndSection: React.FunctionComponent = () => {
         Front-end Tech Stacks
       </Text>
       <SkillPanel
+        tooltip="TypeScript"
+        icon={<TypescriptSvg size={50} />}
+        progressPercent={60}
+        skillExperienceDesc="Nothing better than typed JavaScript ❤️. Used it since January 2019, first on my UUM Math Camp project."
+      />
+      <SkillPanel
+        tooltip="JavaScript"
+        icon={<DiJavascript1 size={50} />}
+        progressPercent={70}
+        skillExperienceDesc="My primary language before switching to TypeScript. Using it since 2018, with HTML and CSS"
+      />
+      <SkillPanel
+        tooltip="HTML"
+        icon={<DiHtml5 size={50} />}
+        progressPercent={65}
+        skillExperienceDesc="Using it since 2018, with JavaScript and CSS."
+      />
+      <SkillPanel
+        tooltip="CSS"
+        icon={<DiCss3 size={50} />}
+        progressPercent={60}
+        skillExperienceDesc="Using it since 2018, with JavaScript and HTML. But now for me Sass/Scss is preferred over CSS."
+      />
+
+      <SkillPanel
         tooltip="React"
         icon={<DiReact size={50} />}
         progressPercent={70}
         skillExperienceDesc="My go-to JavaScript library when building web app. Used it since 2018. First used for serious project in Jan 2019."
       />
       <SkillPanel
-        tooltip="TypeScript"
-        icon={<TypescriptSvg size={50} />}
-        progressPercent={60}
-        skillExperienceDesc="Nothing better than typed JavaScript ❤️. Used it since January 2019, first on my UUM Math Camp project."
+        tooltip="Vue"
+        icon={<FaVuejs size={50} />}
+        progressPercent={30}
+        skillExperienceDesc="Used in once my first project in UUM Math Camp. Not very proficient with it compared to React."
       />
       <SkillPanel
         tooltip="Angular"

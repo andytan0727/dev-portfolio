3ffcb437a85082de073a50c32af34b8ce6387920
Andy Tan <andytan0727@gmail.com>
2019-09-27 15:39:08 +0800

    refactor: make about me more concise and shorter

--- src/page-sections/about-me.tsx
+++ src/page-sections/about-me.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { Element } from "react-scroll";
-import { Box, Flex, Text } from "rebass";
+import { Box, Text, TextProps } from "rebass";
 import { useThemeUI } from "theme-ui";
 
-import { Avatar } from "@components/image";
+import styled from "@emotion/styled";
 import { HighlightText, SectionTitle } from "@src/components/text";
+
+// rebass components styles
+// to ensure styles consistency across different components
+const textVerticalSpacing = [4, 4];
+const textLineHeight = 2;
+const textFontSize = 3;
+
+const StyledUl = styled.ul`
+  display: grid;
+  grid-auto-rows: min-content;
+  grid-template-columns: 100%;
+  grid-row-gap: 1rem;
+`;
+
+const AboutMeParagraphText: React.FunctionComponent<TextProps> = (
+  props: TextProps
+) => {
+  const { children, ...rest } = props;
+
+  return (
+    <Text
+      {...rest}
+      fontSize={textFontSize}
+      lineHeight={textLineHeight}
+      mt={textVerticalSpacing}
+    >
+      {children}
+    </Text>
+  );
+};
 
 const AboutMe: React.FunctionComponent = () => {
   const { colorMode } = useThemeUI();
@@ -12,8 +42,9 @@ const AboutMe: React.FunctionComponent = () => {
   return (
     <Element name="about-me">
       <Box
-        px={[0, 3]}
-        py={4}
+        px={[0, 4]}
+        // py={4}
+        py={textVerticalSpacing}
         sx={{
           minHeight: "100vh",
           display: "grid",
@@ -31,17 +62,6 @@ const AboutMe: React.FunctionComponent = () => {
           backgroundColor: colorMode === "dark" ? "purple3" : "purple1",
         }}
       >
-        <Flex
-          width={1}
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <SectionTitle mt={[0, 0]}>About Me</SectionTitle>
-          <Box mt={4}>
-            <Avatar />
-          </Box>
-        </Flex>
         <Box
           height="100%"
           sx={{
@@ -50,97 +70,59 @@ const AboutMe: React.FunctionComponent = () => {
             justifySelf: ["center", "center", "start"],
           }}
         >
-          <Text mt={3} fontSize={3} lineHeight={1.7}>
-            I am Andy, with the full name Tan Boon Ping, currently pursuing a
-            degree of Bachelor of Science With Honours (Business Mathematics)
-            [B.Sc. (Hons) Business Mathematics] in Universiti Utara Malaysia
-            (UUM). During my university years, I have stumbled upon computer
-            programming in Introduction to Programming course, with{" "}
-            <HighlightText text="Java" /> as my first computer language. After
-            that, I developed much interest and devoted most of my time into
-            this deep field.
-            <br />
-            <br />
-            {""}After going through almost a year of using Java for some basic
-            programming stuffs, at early 2018, I started to dive into the field
-            of <HighlightText text="data science and machine learning" />. At
-            the same time, my passion with previously not so interested{" "}
-            <HighlightText text="linux" /> operating system is burning hot{" "}
-            <span role="img" aria-label="fire">
-              🔥
-            </span>
-            . During that period, I am using{" "}
-            <HighlightText
-              color="muted"
-              backgroundColor="primary"
-              text="Python"
-            />{" "}
-            and learning{" "}
-            <HighlightText
-              color="muted"
-              backgroundColor="primary"
-              text="shell scripting"
-            />{" "}
-            on a daily basis.
-            <br />
-            <br />
-            {""}And then, around March 2018, I was requested by my friend to
-            lend a helping hand on a game organized by UUM Math Camp. From that
-            time, I started to dig into the world of front-end web technologies;
-            the world of <HighlightText text="HTML, CSS, JavaScript" />. For my
-            first JS project, I choose to use{" "}
-            <HighlightText color="muted" backgroundColor="primary" text="Vue" />{" "}
-            as it is my first framework learnt in front-end web development.
-            After the project ended, I started to dive into another view library
-            by Facebook,{" "}
-            <HighlightText
-              color="muted"
-              backgroundColor="primary"
-              text="React"
-            />
-            . Fast forward to February 2019, after possessing certain
-            proficiency in React, I choose it over Vue as a base for my second
-            project in UUM Math Camp.
-            <br />
-            <br />
-            After accomplished Math Camp project, I started my second project
-            with React, <HighlightText text="Shuffler" />. It is a project made
-            by using React with Google&apos;s view library{" "}
-            <HighlightText
-              color="muted"
-              backgroundColor="primary"
-              text="Material UI"
-            />{" "}
-            . It is written in JavaScript at first, but then I decided to
-            refactor it into React +{" "}
-            <HighlightText
-              color="muted"
-              backgroundColor="primary"
-              text="TypeScript"
-            />
-            <span role="img" aria-label="love">
-              ❤️
-            </span>{" "}
-            at July 2019 as it scales big. Besides React, I have also tried{" "}
-            <HighlightText
-              color="muted"
-              backgroundColor="primary"
-              text="Angular"
-            />{" "}
-            for an event page with UUM SQS college, but the idea is then
-            rejected due to some problems on back-end side.
-            <br />
-            <br />
-            Apart from front-end technologies, I have also several experiences
-            with back-end technologies as well. For example,{" "}
-            <HighlightText text="NodeJs with express framework" />,{" "}
-            <HighlightText text="Python Django" />, and lastly{" "}
-            <HighlightText text="PHP with Laravel" />. Even though I am not as
-            competent as what I am in front-end side, but I am still confident
-            that I have possessed at least basic but not so beginner level of
-            proficiency in back-end field. I am learning back-end technologies
-            mostly due to my interest.
-          </Text>
+          <SectionTitle width="9rem">About Me</SectionTitle>
+
+          <AboutMeParagraphText>
+            I am Andy Tan Boon Ping, currently pursuing a degree of Bachelor of
+            Science With Honours (Business Mathematics) [B.Sc. (Hons) Business
+            Mathematics] in Universiti Utara Malaysia (UUM). I have learned
+            programming and web development skills alongside with my primary
+            study in university. Below is some brief summary of my past
+            experiences:
+          </AboutMeParagraphText>
+          <AboutMeParagraphText>
+            <StyledUl>
+              <li>
+                First learnt programming in the year 2017. Get to know about web
+                development in the lower half of the year.
+              </li>
+              <li>
+                Dive deeper into web development starting from the year 2018.
+                During the period, I developed my first project with{" "}
+                <HighlightText text="Vue" />, which is a game for primary school
+                kids in UUM Math Camp 2018 event.
+              </li>
+              <li>
+                Fast forward to the year 2019, I developed my second project
+                with UUM Math Camp 2019, which the project required me to make a
+                board game in conjunction with other games in the event. The
+                board game is created by using <HighlightText text="React" />{" "}
+                and <HighlightText text="TypeScript" />
+              </li>
+              <li>
+                In the same year, I started my hobby project, Shuffler,
+                requested by my friend. Shuffler is an app that randomizes user
+                existing YouTube playlist and play it. It starts simple with
+                React, then refactored into TypeScript in July for better
+                maintainability.
+              </li>
+              <li>
+                After Shuffler, I get my hands on some other projects by using
+                other tech/languages as well, such as notes-gallery (
+                <HighlightText text="PHP" />
+                ), Newster (
+                <HighlightText text="Go" /> &amp; <HighlightText text="Vue" />)
+                and so on. More projects are shown in my github repository.
+              </li>
+            </StyledUl>
+          </AboutMeParagraphText>
+
+          <AboutMeParagraphText>
+            During my free time, I am constantly keeping myself updated with the
+            latest technology in front-end world as well. Besides front-end app,
+            I started to dive into the backend world for a better understanding
+            on the overall working mechanism of the web.
+          </AboutMeParagraphText>
         </Box>
       </Box>
     </Element>

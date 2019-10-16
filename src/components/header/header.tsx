2fc299953316fd4d76d429dd8306218d6668c066
Andy Tan <andytan0727@gmail.com>
2019-10-16 21:46:07 +0800

    refactor: remove border bottom for nav items
    Border bottom of active nav item is unstable across different browsers.
    Hence the decision of dropping border bottom for active nav items.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link, scrollSpy } from "react-scroll";
 import { Box, Button, Flex, Text } from "rebass";
@@ -16,15 +16,6 @@ const StyledLink = styled(Link)`
   font-size: 1.2rem;
   cursor: pointer;
 `;
-
-const scrollOptions = {
-  smooth: "easeInOutQuart",
-  spy: true,
-  hashSpy: true,
-  duration: 500,
-  offset: -75, // offset height of navbar
-  activeClass: "activeLink",
-};
 
 const sectionLinks: SectionLinks = [
   {
@@ -49,8 +40,14 @@ const Header: React.FunctionComponent = () => {
   const [mode, setMode] = useColorMode();
   const [isTablet, setIsTablet] = useState(false);
   const [showNav, setShowNav] = useState(false);
+  const [scrollOptions, setScrollOptions] = useState({
+    smooth: "easeInOutQuart",
+    duration: 550,
+  });
   const flexDirection = isTablet ? "column" : "row";
   const navItemFontSize = [2, 3];
+
+  const navRef = useRef<HTMLDivElement>(null);
 
   const handleChangeMode = useCallback(() => {
     const next = mode === "dark" ? "light" : "dark";
@@ -70,6 +67,15 @@ const Header: React.FunctionComponent = () => {
 
     // check screen size and resize accordingly once on mount
     handleResize();
+
+    // set offset height to scroll into section
+    const curRef = navRef.current;
+    if (curRef) {
+      setScrollOptions(prevOptions => ({
+        ...prevOptions,
+        offset: curRef.offsetHeight * -1 + 1, // offset height of navbar
+      }));
+    }
   }, [handleResize]);
 
   const handleToggleShowNav = useCallback(() => {
@@ -80,6 +86,7 @@ const Header: React.FunctionComponent = () => {
 
   return (
     <Flex
+      ref={navRef}
       px={[0, 4]}
       py={3}
       width={1}
@@ -89,15 +96,7 @@ const Header: React.FunctionComponent = () => {
         position: "fixed",
         backgroundColor: "background",
         zIndex: 2,
-
-        // disable border bottom on tablet size and below
-        "& .activeLink": !isTablet
-          ? {
-              borderBottomWidth: "5px",
-              borderBottomStyle: "solid",
-              borderBottomColor: "primary",
-            }
-          : "",
+        fontWeight: "bold",
       }}
     >
       <StyledLink to="landing" {...scrollOptions}>
@@ -141,6 +140,7 @@ const Header: React.FunctionComponent = () => {
             mt={[2, 0]}
             px={[2, 3]}
             py={[1, 2]}
+            fontWeight="inherit"
             onClick={handleChangeMode}
           >
             {mode}

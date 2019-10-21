4a696871e32d9ef466746c0e2a2f2bf13ea8d4d7
Andy Tan <andytan0727@gmail.com>
2019-10-21 23:55:04 +0800

    refactor: add Go to skills

--- src/page-sections/skills/back-end.tsx
+++ src/page-sections/skills/back-end.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DiLaravel, DiNodejsSmall, DiPhp } from "react-icons/di";
+import { DiGo, DiLaravel, DiNodejsSmall, DiPhp } from "react-icons/di";
 import { Box, Text } from "rebass";
 
 import { SkillPanel } from "@src/components/panel";
@@ -20,15 +20,22 @@ const BackEndSection: React.FC = () => {
       <SkillPanel
         tooltip="Core PHP"
         icon={<DiPhp size={50} />}
-        progressPercent={45}
+        progressPercent={25}
         skillExperienceDesc="Using it since June 2019, when learning Laravel."
       />
 
       <SkillPanel
         tooltip="Laravel"
         icon={<DiLaravel size={50} />}
-        progressPercent={45}
+        progressPercent={25}
         skillExperienceDesc="My main go-to backend framework for PHP. Using it since June 2019."
+      />
+
+      <SkillPanel
+        tooltip="Go"
+        icon={<DiGo size={50} />}
+        progressPercent={35}
+        skillExperienceDesc="Learning to use Go starting from August 2019. First on Newster"
       />
     </Box>
   );
